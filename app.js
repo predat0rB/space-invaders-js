99b56afbb8261b0cd5df3264094ea9ddee6d574d
Jared <rapidtazzz@gmail.com>
2020-05-26 19:40:01 -0400

    Create style.css Create classes for drawable and background. Create game class that holds all data in context.

---
+++ app.js
@@ -0,0 +1,100 @@
+// Initialize game and start.
+var game = new Game();
+function init(){
+    if(game.init()){
+        game.start();
+    }
+}
+
+// Creating drawable object. This will be base oject for all drawable objects in the game.
+function Drawable() {
+    this.init = function(x, y)  {
+        this.x = x;
+        this.y = y;
+    }
+    
+    this.speed = 0;
+    this.canvasWidth = 0;
+    this.canvasHeight = 0;
+
+    // This is our draw function
+    this.draw = function(){
+
+    }
+}
+
+
+// object to hold all images for the game. This function insures images are only ever created once.
+var imageRepo = new function(){
+
+    // Defining images.
+    this.background = new Image();
+
+    // setting image sources
+    this.background.src = "./imgages/bg.png";
+}
+// console.log(imageRepo);
+
+
+// This creates backround object. Background is drawn on canvas, and creats illusion that it is moving.
+function Background(){
+    this.speed = 1;
+
+    // Define the draw function for our background drawable.
+    this.draw = function(){
+        // This is where were gonna pan the background.
+        this.y += this.speed;
+        this.context.drawImage(imageRepo.background, this.x, this.y);
+        // Draw another image at the top edge of the first image.
+        this.context.drawImage(imageRepo.background, this.x, this.y - this.canvasHeight);
+        // checking to see if the image scrolled off the screen, if it did, reset.
+        if (this.y >= this.canvasHeight){
+            this.y = 0;
+        }
+    }
+}
+Background.prototype = new Drawable();
+
+
+// This is our game object. Will hold all data for everything in the game.
+function Game(){
+    
+    this.init = function(){
+        // Going to get canvas context to setup game objects. Will return true if browser supports canvas.
+        this.backgroundCanvas = document.getElementById('background');
+        // If the canvas is supported.
+            if(this.backgroundCanvas.getContext){
+    
+                this.backgroundContext = this.backgroundCanvas.getContext('2d');
+                // Going to initialize objects to contain context and the canvas information.
+                Background.prototype.context = this.backgroundContext;
+                Background.prototype.canvasWidth = this.backgroundCanvas.width;
+                Background.prototype.canvasHeight = this.backgroundCanvas.height;
+                // Initializing background objects.
+                this.background = new Background();
+                this.background.init(0,0);
+                return true;
+            }
+            else{
+                return false;
+            }
+    }
+    // This will be starting animation loop.
+    this.start = function(){
+        animate();
+    }
+}
+function animate() {
+    requestAnimFrame( animate );
+    game.background.draw();
+}
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame   ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            function(/* function */ callback, /* DOMElement */ element){
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
